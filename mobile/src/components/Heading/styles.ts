6d5ab7a5abe90bc698644a1c0789745f400923c5
Débora Ferreira <devsouza.root@gmail.com>
2022-09-13 22:38:25 -0300

    High speed - Completo

---
+++ mobile/src/components/Heading/styles.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+import { THEME } from '../../theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 32,
+    paddingLeft: 35,
+    marginTop: 35,
+  }, 
+  title: {
+    color: THEME.COLORS.TEXT,
+    fontSize: THEME.FONT_SIZE.LG,
+    fontFamily: THEME.FONT_FAMILY.BLACK,
+  },
+  subtitle: {
+    color: THEME.COLORS.CAPTION_400,
+    fontSize: THEME.FONT_SIZE.MD,
+    fontFamily: THEME.FONT_FAMILY.REGULAR,
+  },
+});
