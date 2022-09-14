13706ce1a9bc88040c4f8af7c48c1e1051ae1f2b
Débora Ferreira <59145479+Debora7376@users.noreply.github.com>
2022-09-14 16:58:45 -0300

    To be continued parte 1

---
+++ server/README.md
@@ -0,0 +1,29 @@
+# Back-end
+
+## Entidades
+
+### Game
+
+id
+title
+bannerUrl
+
+
+### Ad
+
+id
+gameId
+name
+yearsPlaying
+weekDays
+hoursStart
+hoursEnd
+useVoiceChannel
+createdAt
+
+## Caso de uso 
+
+- Listagem de games com contagem de anuncios
+- Criação de novo anuncio
+- Listagem de anuncios por game
+- Buscar discord pelo ID do anuncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,25 @@
-import express from 'express';
+import express, { request, response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
+const prisma = new PrismaClient();
+
+// HTTP methods / API RESTful / HTTP Codes 
+
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany()
+
+  return response.json(games);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: 'Anúncio 1'},
     { id: 2, name: 'Anúncio 2'},
@@ -10,6 +27,12 @@ app.get('/ads', (request, response) => {
     { id: 4, name: 'Anúncio 4'},
     { id: 5, name: 'Anúncio 5'},
   ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  // const adId = request.params.id;
+  
+  return response.json([])
 })
 
 app.listen(3333);
